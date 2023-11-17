d598765a92b21c8a7c621a402d4440056e5d59dd
Durgapavankumarrithub <150928825+Durgapavankumarrithub@users.noreply.github.com>
2023-11-17 21:04:21 +0530

    comparing two numbers

---
+++ Variables_ex.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Variables_ex {
+ public static void main(String[] args) 
+ {
+	 Scanner sc=new Scanner(System.in);
+	int number1=sc.nextInt();
+	int number2=sc.nextInt();
+	if(number1==number2) 
+	{
+		System.out.println("number1 and number2 are equal");
+	}
+	else
+	{
+		System.out.println("number1 and number2 are not equal");
+	}
+}
+}
