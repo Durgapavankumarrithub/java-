85a0d16727f3fe275b4d1b14b95620a11f1894ef
Durgapavankumarrithub <150928825+Durgapavankumarrithub@users.noreply.github.com>
2023-11-18 13:06:53 +0530

    display variables of different datatypes

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+public static void main(String[] args) {
+	byte age=22;
+	long contact=9392585888l;
+	float salary=25000.25458f;
+	int rent=6000;
+	double Emi=10000.89895809309;
+	boolean is_rainy=true;
+	char sex='M';
+	System.out.println("age:"+age);
+	System.out.println("contact:"+contact);
+	System.out.println("salary:"+salary);
+	System.out.println("rent:"+rent);
+	System.out.println("EMI:"+Emi);
+	System.out.println("rainy:"+is_rainy);
+	System.out.println("gender:"+sex);
+	
+	
+}
+}
